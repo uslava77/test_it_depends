3a8d4cde468da9bb6732597c1cead8bbacf68afc
uslava77 <uslava77@gmail.com>
2019-11-21 11:43:13 -0800

    add comment

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # test_it_depends
+# whee
 test repo for It-Depends https://github.com/jyoo980/it-depends
--- hello_world.java
+++ hello_world.java
@@ -1,3 +1,5 @@
+// this is a hello world program
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World");
